7806170f82a050cb379ff5de8ecaf16a79f1a9e5
okabekaito <kaito19991204@gmail.com>
2022-01-12 16:53:49 +0900

    reactとtypescript導入

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TriviaApp</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css')}}">
+    </link>
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+<script src=" {{ mix('js/index.js') }} "></script>
+
+</html>
