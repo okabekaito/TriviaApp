b03c0bcc6edd3535e43803e339fc2fc1c1e0f188
okabekaito <kaito19991204@gmail.com>
2022-01-15 17:31:52 +0900

    API取得1

--- resources/ts/components/pages/posts/PostPage.tsx
+++ resources/ts/components/pages/posts/PostPage.tsx
@@ -1,10 +1,32 @@
-import React, { VFC } from "react"
-
+import React, { useCallback, useEffect, useState, VFC } from "react"
+import axios from "axios";
 
 export const PostPage : VFC = () => {
+    
+    const [posts, setPosts] = useState([]);
+
+    const [formData,setFormData] = useState({title:'',category:'',content:''});
+
+    useEffect(() =>  {
+        getPostsData();
+    },[])
+
+    const getPostsData = () => {
+        axios
+            .get('api/posts')
+            .then(response => {
+                setPosts(response.data);
+                console.log(response.data)
+            })
+            .catch(() => {
+                console.log('通信に失敗しました');
+            });
+    }
+
     return (
         <>
             <h1>Post Page</h1>
+
         </>
     )
 }
