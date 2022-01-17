bd2faea550ed590e7669d6d06e382f85629b6707
okabekaito <kaito19991204@gmail.com>
2022-01-17 22:06:09 +0900

    axios post 1

--- resources/ts/components/pages/posts/PostPage.tsx
+++ resources/ts/components/pages/posts/PostPage.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect, useState, VFC } from "react"
+import React, { ChangeEvent, useEffect, useState, VFC } from "react"
 import axios from "axios";
-import { Box, Button, Input, InputGroup, InputRightElement, Select, Stack, Textarea } from "@chakra-ui/react";
+import { Button, FormControl, Input, Select, Stack, Textarea } from "@chakra-ui/react";
 import {
     Table,
     Thead,
@@ -26,7 +26,9 @@ export const PostPage : VFC = () => {
     
     const [posts, setPosts] = useState<Post[]>([]);
 
-    const [formData,setFormData] = useState({title:'',category:'',content:''});
+    const [formData,setFormData] =useState<string>();
+
+
 
     useEffect(() =>  {
         getPostsData();
@@ -43,6 +45,23 @@ export const PostPage : VFC = () => {
             });
     }
 
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+      setFormData(e.target.value);
+    }
+
+    const createPost = async(title:string,category:string,content:string) => {
+      //入力値を投げる
+      const {data} =  await axios.post<Post>('/api/posts',{title:title,category:category,content:content})
+      return data
+
+  }
+
+
+    const onClickPost = (e:React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      console.log(formData)
+    }
+    
 
 
 
@@ -51,6 +70,7 @@ export const PostPage : VFC = () => {
         <>
             <h1>Post Page</h1>
             <Stack spacing={3}>
+              <FormControl  onSubmit={handleChange}>
   <Input variant='outline' placeholder='タイトル' />
   <Select placeholder='カテゴリーを選択'>
   <option value='option1'>家庭</option>
@@ -60,9 +80,11 @@ export const PostPage : VFC = () => {
   <option value='option5'>人間関係</option>
   <option value='option6'>その他</option>
 </Select>
-  <Textarea placeholder='内容' />
+  <Textarea placeholder='内容'  />
   <Button>投稿</Button>
+  </FormControl>
 </Stack>
+
             <Table variant='simple'>
               
   <TableCaption>Imperial to metric conversion factors</TableCaption>
